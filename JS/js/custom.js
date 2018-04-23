99fd87d0406751db70514ef2aa143fe75473bf06
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/js/custom.js
@@ -0,0 +1,90 @@
+﻿var store = new DevExpress.data.ArrayStore({
+    key: 'Id',
+    data: [
+        { Id: 1, Category: 'Oceania', VisibleIndex: 0 },
+        { Id: 2, Category: 'Africa', VisibleIndex: 1 },
+        { Id: 3, Category: 'Americas', VisibleIndex: 2 },
+        { Id: 4, Category: 'Asia', VisibleIndex: 3 },
+        { Id: 5, Category: 'Europe', VisibleIndex: 4 }
+    ]
+});
+function initDragging ($gridElement) {
+    $gridElement.find('.myRow').draggable({
+        helper: 'clone',
+        start: function (event, ui) {
+            var $originalRow = $(this),
+                $clonedRow = ui.helper;
+            var $originalRowCells = $originalRow.children(),
+                $clonedRowCells = $clonedRow.children();
+            for (var i = 0; i < $originalRowCells.length; i++)
+                $($clonedRowCells.get(i)).width($($originalRowCells.get(i)).width());
+            $clonedRow
+              .width($originalRow.width())
+              .addClass('drag-helper');
+        }
+    });
+    $gridElement.find('.myRow').droppable({
+        drop: function (event, ui) {
+            var draggingRowKey = ui.draggable.data('keyValue');
+            var targetRowKey = $(this).data('keyValue');
+            var draggingIndex = null,
+                targetIndex = null;
+            store.byKey(draggingRowKey).done(function (item) {
+                draggingIndex = item.VisibleIndex;
+            });
+            store.byKey(targetRowKey).done(function (item) {
+                targetIndex = item.VisibleIndex;
+            });
+            var  draggingDirection = (targetIndex < draggingIndex) ? 1 : -1;
+            var dataItems = null
+            store.load().done(function (data) {
+                dataItems = data;
+            });
+            for (var dataIndex = 0; dataIndex < dataItems.length; dataIndex++) {
+                if ((dataItems[dataIndex].VisibleIndex > Math.min(targetIndex, draggingIndex)) && (dataItems[dataIndex].VisibleIndex < Math.max(targetIndex, draggingIndex))) {
+                    dataItems[dataIndex].VisibleIndex += draggingDirection;
+                }
+            }
+            store.update(draggingRowKey, { VisibleIndex: targetIndex});
+            store.update(targetRowKey, { VisibleIndex: targetIndex + draggingDirection });
+            $gridElement.dxDataGrid('instance').refresh();
+        }
+    });
+}
+var viewModel = {
+    gridSettings: {
+        dataSource: {
+            store: store,
+            sort: 'VisibleIndex'
+        },       
+        columns: [
+            {
+                dataField: 'Id',
+                allowSorting: false
+            },
+            {
+                dataField: 'Category',
+                allowSorting: false
+            },
+            { 
+                dataField: 'VisibleIndex',
+                caption: 'Index',
+                allowSorting: false
+            }
+        ],
+        onRowPrepared: function (e) {
+            if (e.rowType != 'data')
+                return;
+            e.rowElement
+            .addClass('myRow')
+            .data('keyValue', e.key);
+        },
+        loadPanel: {
+            enabled: false
+        },
+        width: 400,
+        onContentReady: function (e) {
+            initDragging(e.element);
+        }
+    }
+};
